ca347d13685dc9960940397964cda1884fc8aef1
Tom Schwarz <hi@tomschwarz.info>
2020-10-02 13:41:52 +0200

    Add TODO

--- include/helper/helper.h
+++ include/helper/helper.h
@@ -23,11 +23,11 @@ string trim(const string& s);
 
 string subStrWithEndingDots(const string& s, const unsigned int length);
 
+// TODO: Put these functions to ConfigReader as static methods
 string getConfigHomePath();
 string getConfigPathByName(const string& name);
 string getMainConfigPath();
 string getFeedConfigPath();
-
 bool configFilesExists();
 bool copyDefaultConfigFiles();
 
